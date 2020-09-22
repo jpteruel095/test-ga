efa5024c698db6bbc7c18585f98a3e4a7ef57960
John Patrick <jpteruel95@gmail.com>
2020-09-22 17:53:03 +0800

    Initialized Xcode Project

---
+++ Arventa ConnectTests/Arventa_ConnectTests.swift
@@ -0,0 +1,33 @@
+//
+//  Arventa_ConnectTests.swift
+//  Arventa ConnectTests
+//
+//  Created by John Patrick Teruel on 9/22/20.
+//
+
+import XCTest
+@testable import Arventa_Connect
+
+class Arventa_ConnectTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
---
+++ Podfile
@@ -0,0 +1,33 @@
+# Uncomment the next line to define a global platform for your project
+# platform :ios, '9.0'
+
+target 'Arventa Connect' do
+  # Comment the next line if you don't want to use dynamic frameworks
+  use_frameworks!
+
+  # Pods for Arventa Connect
+  pod 'Alamofire' # API Communicator
+  pod 'SwiftyJSON' # Handling JSON Data from API
+  pod 'SwiftDate'
+  pod 'ObjectMapper'
+  pod 'ReachabilitySwift'
+  
+  #UI
+  pod 'IBAnimatable'
+  pod 'IQKeyboardManagerSwift'
+  pod 'MBProgressHUD'
+  
+  # Firebase
+  pod 'Firebase/Analytics'
+  pod 'Firebase/Crashlytics'
+  
+  target 'Arventa ConnectTests' do
+    inherit! :search_paths
+    # Pods for testing
+  end
+
+  target 'Arventa ConnectUITests' do
+    # Pods for testing
+  end
+
+end
