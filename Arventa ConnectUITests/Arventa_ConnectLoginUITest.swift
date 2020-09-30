4a863aa9fd2841ecaa63e30ae9e3b1867ab4b9a5
John Patrick <jpteruel95@gmail.com>
2020-09-30 21:04:56 +0800

    Added test for verification (all in one to prevent blocked accounts)

--- Arventa ConnectUITests/Arventa_ConnectLoginUITest.swift
+++ Arventa ConnectUITests/Arventa_ConnectLoginUITest.swift
@@ -404,4 +404,93 @@ class Arventa_ConnectLoginUITest: XCTestCase {
             LoginTestHelper.validateLoginPage()
         }
     }
+    
+    
+    /**
+     - Description: All Verification tests (to avoid getting locked)
+     - 1 - 1.02
+     */
+    func test_TC1a02t09_LoginVerification() throws {
+        // UI tests must launch the application that they test.
+        let app = XCUIApplication()
+        app.launch()
+        
+        if !LoginTestHelper.didForceLogout(){
+            LoginTestHelper.validateLoginPage()
+        }
+        
+        // Initially, the default selected app is always WHS Monitor
+        TestHelpers.tapOverStaticText("WHS Monitor")
+        TestHelpers.expectActionSheet(title: "Which app would you like to log into?",
+                                      andTap: "Store Manifest",
+                                      inTestCase: self)
+        
+        XCTAssert(app.staticTexts["Store Manifest"].exists)
+        
+        LoginTestHelper.enterCredentialsAndTapLogin(username: "sm_numlock1",
+                                                    password: "watsoN#12345")
+        
+        // Test Case 1.2 - Testing access to verification screen after valid login
+        let verifyLabel = app.staticTexts["Verify your account"]
+        let exists = NSPredicate(format: "exists == 1")
+        expectation(for: exists, evaluatedWith: verifyLabel, handler: nil)
+        waitForExpectations(timeout: 10, handler: nil)
+        XCTAssert(app.staticTexts["Code is sent to ********2544"].exists)
+        
+        app.textFields.firstMatch.tap()
+        LoginTestHelper.enterOTPCode(code: "123123")
+        
+        // Test Case 1.5 - Testing error message when entering invalid code
+        app.toolbars["Toolbar"].buttons["Done"].tap()
+        
+        app.buttons["CONTINUE"].tap()
+        TestHelpers.expectAlert(withTitle: "Error",
+                                andAssertMessage: "Invalid Security Code.",
+                                thenTap: "OK",
+                                butWait: true,
+                                inTestCase: self)
+        
+        XCTAssert(app.staticTexts["Code is sent to ********2544"].exists)
+        
+        // Test Case 1.6 - Testing error message when attempting to verify while offline
+        TestHelpers.executeWhileOffline {
+            app.textFields.firstMatch.tap()
+            
+            //This assumes that the OTP is really not 123123
+            LoginTestHelper.enterOTPCode(code: "123123")
+            app.toolbars["Toolbar"].buttons["Done"].tap()
+            
+            app.buttons["CONTINUE"].tap()
+            TestHelpers.expectAlert(withTitle: "Error",
+                                    andAssertMessage: "You are currently offline.",
+                                    thenTap: "OK",
+                                    butWait: true,
+                                    inTestCase: self)
+            
+            // Segue for Test Case 1.8 -
+            // Testing error message when attempting to send the code while offline
+            app.buttons["Resend code"].tap()
+            TestHelpers.expectAlert(withTitle: "Error",
+                                    andAssertMessage: "You are currently offline.",
+                                    thenTap: "OK",
+                                    butWait: true,
+                                    inTestCase: self)
+        }
+        
+        XCTAssert(app.staticTexts["Code is sent to ********2544"].exists)
+        
+        // Test Case 1.7 - Testing alert message when attempting to send the code
+        app.buttons["Resend code"].tap()
+        TestHelpers.expectAlert(withTitle: "Success",
+                                andAssertMessage: "The code has been sent.",
+                                thenTap: "OK",
+                                butWait: true,
+                                inTestCase: self)
+        
+        XCTAssert(app.staticTexts["Code is sent to ********2544"].exists)
+        
+        // Test Case 1.9 - Testing action when tapping back button
+        app.buttons["backButton"].tap()
+        LoginTestHelper.validateLoginPage(emptyFields: false)
+    }
 }
